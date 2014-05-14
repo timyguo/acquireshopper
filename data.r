88483734d5c345609d03ebaed902e30c283c7f8e
timyguo <tim.y.guo@gmail.com>
2014-05-14 23:09:12 +1000

    Update

--- data.r
+++ data.r
@@ -5,13 +5,13 @@ con <- dbConnect(drv, host='192.168.1.7', port=5432, user='postgres', password='
 
 trans <- fetch(dbSendQuery(con, 'select * from model.trans'), n=-1)
 
-testHistory <- fread('data/testHistory.csv', colClasses=rep('numeric',5))
+testHistory <- fread('data/testHistory.csv', colClasses=c('factor',rep('numeric',4)))
 testHistory[,offerdate:=as.Date(offerdate)]
-trainHistory <- fread('data/testHistory.csv', colClasses=rep('numeric',5))
+trainHistory <- fread('data/trainHistory.csv', colClasses=c('factor',rep('numeric',6)))
 trainHistory[,offerdate:=as.Date(offerdate)]
 offers <- fread('data/offers.csv', colClasses=rep('numeric',6))
-sampleSubmission <- fread('data/sampleSubmission.csv', colClasses=rep('numeric',2))
-trans <- fread('data/trans.csv', colClasses=rep('numeric',11))
+sampleSubmission <- fread('data/sampleSubmission.csv', colClasses=c('factor',rep('numeric',1)))
+trans <- fread('data/trans.csv', colClasses=c('factor',rep('numeric',10)))
 trans[,date:=as.Date(date)][,productmeasure:=factor(productmeasure)]
 
 save(testHistory, trainHistory, offers, sampleSubmission, trans, file='modeldata.rdata')
--- features.r
+++ features.r
@@ -1,7 +1,17 @@
 load('data/modeldata.rdata')
 
 setkey(trans, id)
+names(offers) <- c('offer', paste('offers',names(offers)[-1], sep='.'))
+trainHistory <- merge(trainHistory, offers, by='offer')
 
-trans[, lastndays:=tail(date,1)-date, by=id]
+trans <- merge(trans, trainHistory, by=c('id','chain'))
+trans[,beforeoffer:=date<offerdate]
+trans[, ndaybeforeoffer:=offerdate-date,by=id]
 
-#
+save(trans, file='data/trans.rdata')
+
+generaten <- function(days, category, offer.category,n){
+    return(sum(days<=n&category==offer.category))
+}
+
+features.trans <- trans[ndaybeforeoffer>0, paste('category_bought',c(30,60,90)),apply(c(30,60,90),generaten(ndaybeforeoffer, category, offer.category),by=id]
